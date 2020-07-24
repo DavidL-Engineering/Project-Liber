2744c6e7429e2d021e6689ebed5d1945597e6722
David Leong <davidtoronto18@gmail.com>
2020-07-24 16:39:28 -0400

    New files and changes to match iteration exporting capabilities

--- test.py
+++ test.py
@@ -1,19 +1,10 @@
-import gspread
-from pprint import pprint
+results_directory = "C:/Users/david/Documents/1_(SSD)_Important_Things/Work/Project Liber/Results"
+files_directory = "C:/Users/david/Documents/1_(SSD)_Important_Things/Work/Project Liber/Data"
 
-Title_Line = 32
-sim_numbers = [15,16,17,18]
+iter_file = open("{}/drag-rfile.out".format(files_directory), 'r')
+iter_lines = iter_file.readlines()
+iter_data = iter_lines[len(iter_lines)-1]
 
-s_sheet_title = "Google API Test"
-w_sheet_title = "DOE Summary Results"
-
-gc = gspread.oauth()
-
-spreadsheet = gc.open("{}".format(s_sheet_title))
-w_sheet = spreadsheet.worksheet("{}".format(w_sheet_title))
-
-sim_row = w_sheet.findall("15", in_column=1)
-for i in sim_row:
-    print("row = " + str(i.row))
-    print("col = " + str(i.col))
-
+output = open("{}/iter{}.txt".format(results_directory, 0), 'w')
+output.write(iter_data)
+output.close()
