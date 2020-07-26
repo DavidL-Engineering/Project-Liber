1fc7f3a6407cf40faa60087f292e901da0dc433d
David Leong <davidtoronto18@gmail.com>
2020-07-26 16:41:09 -0400

    Fixed bugs, added automatic file extension placing

--- Automatic Archive.py
+++ Automatic Archive.py
@@ -9,12 +9,12 @@ SetScriptVersion(Version="19.1.103")
 INSTRUCTIONS:
 1. Enter the project directories under proj_dir. Change all backslashes to forward slashes
 e.g. "C:/Users/tom/Documents/DV2 Nose Study/Group 3"
-2. Enter the project name and file extension under proj_name.
-e.g. "test.wbpj"
+2. Enter the project name under proj_name.
+e.g. "Test"
 3. Enter the design series of the project under series. The series you may choose from are: V1, V2, V3, V4, V5, V6, DV1, DV2, DV3, DV4. Do not include any spaces, and capitalize all letters.
 e.g. "DV1"
-4. Enter the desired name of the project's archive and the file extension under archive_name.
-e.g. "test.wbpz"
+4. Enter the desired name of the project's archive under archive_name.
+e.g. "Test Archive"
 '''
 
 proj_dir = [
@@ -38,8 +38,8 @@ archive_name = [
 "INSERT_HERE"
 ]
 
-for i in range(len(wb_dir))
-    Open(FilePath="{}".format(wb_dir[i]))
+for i in range(len(proj_dir)):
+    Open(FilePath="{}/{}.wbpj".format(proj_dir[i], proj_name[i]))
     Archive(
-        FilePath="//172.16.1.12/hpc/sims/Archives/{}/{}".format(series[i], name[i]),
+        FilePath="//172.16.1.12/hpc/sims/Archives/{}/{}.wbpz".format(series[i], archive_name[i]),
         IncludeExternalImportedFiles=True)
