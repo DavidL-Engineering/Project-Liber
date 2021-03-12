0642bc205b33a025c91ec50edb233abff13a14a7
David L <davidtoronto18@gmail.com>
2021-03-11 20:35:58 -0500

    Updated script to account for cycle parent folder

--- Automatic Archive.py
+++ Automatic Archive.py
@@ -11,6 +11,8 @@ INSTRUCTIONS:
 e.g. "C:/Users/tom/Documents/DV2 Nose Study/Group 3"
 2. Enter the project name under proj_name.
 e.g. "Test"
+3. Enter the cycle the project belongs to under cycle. The cycles you may choose from are: Gen X, Gen 11, Gen 12
+e.g. "Gen 11"
 3. Enter the design series of the project under series. The series you may choose from are: V1, V2, V3, V4, V5, V6, DV1, DV2, DV3, DV4, DV5, DV6, DV7. Do not include any spaces, and capitalize all letters.
 e.g. "DV1"
 4. Enter the desired name of the project's archive under archive_name.
@@ -27,6 +29,10 @@ proj_name = [
 "INSERT_HERE"
 ]
 
+cycle = [
+"INSERT_HERE",
+"INSERT_HERE"
+]
 
 series = [
 "INSERT_HERE",
@@ -41,5 +47,5 @@ archive_name = [
 for i in range(len(proj_dir)):
     Open(FilePath="{}/{}.wbpj".format(proj_dir[i], proj_name[i]))
     Archive(
-        FilePath="//172.16.1.12/hpc/sims/Archives/{}/{}.wbpz".format(series[i], archive_name[i]),
+        FilePath="//172.16.1.12/hpc/sims/Archives/{}/{}/{}.wbpz".format(cycle[i], series[i], archive_name[i]),
         IncludeExternalImportedFiles=True)
